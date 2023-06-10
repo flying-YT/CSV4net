c235d9e084a2b94965f8139c241eb369b575bc19
yuki <flying59.yt@gmail.com>
2023-06-10 07:17:27 -0400

    20230610

--- CSV4net/ReadingFile.cs.cs
+++ CSV4net/ReadingFile.cs.cs
@@ -3,6 +3,66 @@
 namespace CSV4net;
 public class ReadingFile
 {
+    private static List<string> ReadFile(string path)
+    {
+        List<string> list = new List<string>();
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                list.Add(line);
+            }
+        }
+        return list;
+    }
+
+    public static List<string[]> GetCSV(string path, char sepalate=',', string encoding="utf-8")
+    {
+        var list = ReadFile(path);
+        var returnList = new List<string[]>();
+        var sepalateList = new List<string>();
+        StringBuilder sb = new StringBuilder();
+        bool isBool = false;
+        foreach(var line in list)
+        {
+            foreach(char c in line)
+            {
+                if(c == sepalate && !isBool)
+                {  
+                    sepalateList.Add(sb.ToString());
+                    sb = new StringBuilder();
+                }
+                else
+                {
+                    sb.Append(c);
+                    if(c == '"')
+                    {
+                        isBool = !isBool;
+                    }
+                }
+            }
+
+            if(sb.Length != 0 && !isBool)
+            {
+                sepalateList.Add(sb.ToString());
+                sb = new StringBuilder();
+
+                var strArray = new string[sepalateList.Count];
+                int index = 0;
+                foreach(var str in sepalateList)
+                {
+                    strArray[index] = str;
+                    index++;
+                }
+                sepalateList = new List<string>();
+                returnList.Add(strArray);
+            }
+        }
+        return returnList;
+    }
+
+/*
     public static List<string> GetCSV(string path)
     {
         bool isNormal = true;
@@ -32,7 +92,7 @@ public class ReadingFile
             return DoubleQuotationCSV(path);
         } 
     }
-
+*/
     private static List<string> NormalCSV(string path)
     {
         List<string> list = new List<string>();
