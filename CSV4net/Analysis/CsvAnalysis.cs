7c35434cc1f4dfa96a24dec49f6c4046a3763ebf
flying-YT <flying.yt59@gmail.com>
2024-02-13 22:58:05 +0900

    add analysis method.

--- CSV4net/Analysis/CsvAnalysis.cs
+++ CSV4net/Analysis/CsvAnalysis.cs
@@ -32,11 +32,85 @@ public class CsvAnalysis
                 foreach(var data in item.value)
                 {
                     var dataList = columDataList[listIndex];
-                    dataList.Add(new DataModel(item.index, data));
+                    dataList.Add(new DataModel(item.index-1, data));
                     listIndex++;
                 }
             }
         }
+    }
+
+    public void SortDescendingOrderIntValue(string columName)
+    {
+        int keyIndex = keyDict[columName];
+
+        int[] indexArray = new int[columDataList[keyIndex].Count];
+        int[] valueArray = new int[columDataList[keyIndex].Count];
+        foreach(var item in columDataList[keyIndex].Select((value, index) => new { value, index }))
+        {
+            indexArray[item.index] = item.value.Index;
+            valueArray[item.index] = int.Parse(item.value.Data.ToString());
+        }
+
+        for(int i=0;i<valueArray.Length;i++)
+        {
+            for(int j=0;j<valueArray.Length;j++)
+            {
+                if (valueArray[i] > valueArray[j])
+                {
+                    int tempValue = valueArray[j];
+                    valueArray[j] = valueArray[i];
+                    valueArray[i] = tempValue;
+
+                    int tempIndex = indexArray[j];
+                    indexArray[j] = indexArray[i];
+                    indexArray[i] = tempIndex;
+                }
+            }
+        }
+
+        CompareSortIndex(IntArrayToList(indexArray), true);
+    }
+
+    public void SortAscendingOrderIntValue(string columName)
+    {
+        int keyIndex = keyDict[columName];
+
+        int[] indexArray = new int[columDataList[keyIndex].Count];
+        int[] valueArray = new int[columDataList[keyIndex].Count];
+        foreach(var item in columDataList[keyIndex].Select((value, index) => new { value, index }))
+        {
+            indexArray[item.index] = item.value.Index;
+            valueArray[item.index] = int.Parse(item.value.Data.ToString());
+        }
+
+        for(int i=0;i<valueArray.Length;i++)
+        {
+            for(int j=0;j<valueArray.Length;j++)
+            {
+                if (valueArray[i] < valueArray[j])
+                {
+                    int tempValue = valueArray[j];
+                    valueArray[j] = valueArray[i];
+                    valueArray[i] = tempValue;
+
+                    int tempIndex = indexArray[j];
+                    indexArray[j] = indexArray[i];
+                    indexArray[i] = tempIndex;
+                }
+            }
+        }
+
+        CompareSortIndex(IntArrayToList(indexArray), true);
+    }
+
+    private List<int> IntArrayToList(int[] array)
+    {
+        List<int> indexList = new List<int>();
+        foreach(int i in array)
+        {
+            indexList.Add(i);
+        }
+        return indexList;
     }
 
     public void SortEqualTextValue(string columName, string text)
@@ -95,15 +169,30 @@ public class CsvAnalysis
         CompareSortIndex(indexList);
     }
 
-    private void CompareSortIndex(List<int> list)
+    private void CompareSortIndex(List<int> list, bool isIndexUpdate=false)
     {
         if(sortIndexList == null)
         {
             sortIndexList = new List<int>(list);
+            return;
+        }
+        
+        List<int> tempList = new List<int>();
+        if(isIndexUpdate)
+        {
+            foreach(int i in list)
+            {
+                foreach(int v in sortIndexList)
+                {
+                    if(i == v)
+                    {
+                        tempList.Add(i);
+                    }
+                }
+            }
         }
         else
         {
-            List<int> tempList = new List<int>();
             foreach(int i in sortIndexList)
             {
                 foreach(int v in list)
@@ -114,12 +203,26 @@ public class CsvAnalysis
                     }
                 }
             }
-            sortIndexList = new List<int>(tempList);
+        }
+        sortIndexList = new List<int>(tempList);
+    }
+
+    public void ResetSortIndex()
+    {
+        sortIndexList = new List<int>();
+        for(int i=0;i<columDataList[0].Count;i++)
+        {
+            sortIndexList.Add(i);
         }
     }
 
     public List<string[]> GetSortData()
     {
+        if(sortIndexList == null)
+        {
+            ResetSortIndex();
+        }
+
         List<string[]> list = new List<string[]>();
 
         string[,] strArray = new string[sortIndexList.Count, keyDict.Count];
